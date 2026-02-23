01a8b65017be1694aa11d70c31a63eb06b08f7c5
Ümit Can Evleksiz <evleksizumitcan@gmail.com>
2026-02-23 22:45:07 +0300

    use magna kraken logo

--- src/constants/company.ts
+++ src/constants/company.ts
@@ -1,6 +1,6 @@
 export const COMPANY_INFO = {
-  website: 'https://magna.so',
-  linkedin: 'https://www.linkedin.com/company/magnatokens',
-  twitter: 'https://x.com/magna_digital',
-  logoUrl: '/magna-logo.png',
-} as const
+	website: "https://magna.so",
+	linkedin: "https://www.linkedin.com/company/magnatokens",
+	twitter: "https://x.com/magna_digital",
+	logoUrl: "/magna-kraken-logo.png",
+} as const;
